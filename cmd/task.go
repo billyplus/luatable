33c4a7aa60e307e6d3534c294acab5b3c746f887
billy <billy@erigame.com>
2018-12-22 22:06:50 +0800

    增加错误收集

--- cmd/task.go
+++ cmd/task.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"github.com/billyplus/luatable"
+	"github.com/billyplus/luatable/encode"
+	"github.com/pkg/errors"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -19,16 +23,49 @@ func NewTask(sheet *WorkSheet, conf ExportConfig) *Task {
 	return task
 }
 
-func (task *Task) Run() error {
+func (task *Task) Run() (err error) {
+	defer func() { // 用defer来捕获到panic异常
+		if r := recover(); r != nil {
+			fmt.Println("from panic")
+			if e, ok := r.(error); ok {
+				err = errors.WithStack(e)
+			} else {
+				err = errors.Errorf("错误：%v", e)
+			}
+		}
+	}()
+
+	// 创建目录
+	if err = os.MkdirAll(task.Config.Path, 0644); err != nil {
+		return err
+	}
+
 	reader := readerFatory(task.Sheet, task.Config.Filter)
 	result, err := reader.ReadAll()
 	if err != nil {
 		return err
 	}
-	return writeStringToFile(result, filepath.Join(task.Config.Path, sheet.Name+"."+task.Config.Format))
-}
 
-func writeStringToFile(value, path string) error {
-	return ioutil.WriteFile(path, []byte(value), 0644)
-
+	var value interface{}
+	err = luatable.Unmarshal("{"+result+"}", &value)
+	if err != nil {
+		return err
+	}
+	var enc encode.EncodeFunc
+	switch task.Config.Format {
+	case "xml":
+		enc = encode.EncodeXML
+	case "json":
+		enc = encode.EncodeJSON
+	default:
+		return errors.Errorf("不支持的格式类型{out.format}=%s", task.Config.Format)
+	}
+	// 编码成json或xml
+	data, err := enc(value)
+	if err != nil {
+		return err
+	}
+	outfile := filepath.Join(task.Config.Path, task.Sheet.Name+"."+task.Config.Format)
+	// 写入文件
+	return ioutil.WriteFile(outfile, data, 0644)
 }
